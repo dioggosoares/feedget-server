374588b6545c5d96ab3f646d220824b35ea76cce
Diogo Soares <diogo.soares@solarview.com.br>
2022-05-06 18:56:58 -0300

    Add port env variable

--- src/server.ts
+++ src/server.ts
@@ -13,6 +13,6 @@ app.use(bodyParser.json({ limit: '50mb' }));
 app.use(express.json());;
 app.use(routes);
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log("Server running");
 })
